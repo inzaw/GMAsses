98c05c677c826d1ddb26771390066595a8475af4
Zeinab Awada <zeinab.awada05@gmail.com>
2020-10-30 00:04:23 +0200

    Changed GitHub Url
    - Used to use lodash repo

--- Containers/Home/index.js
+++ Containers/Home/index.js
@@ -42,9 +42,9 @@ export default class Homepage extends Component<Props>{
   onRefresh=()=>{
      this.setState({ refreshing: true }, async () => {
        try{
-         commits = await request('GET /repos/lodash/lodash/commits')
+         commits = await request('GET /repos/inzaw/GMAsses/commits')
          transformedCommits = CommitsTransformer.backward(commits.data)
-         this.setState({loading:false,commits:transformedCommits,refreshing:false})
+         this.setState({loading: false, commits: transformedCommits, refreshing: false})
        }
        catch (e){
          this.setState({ refreshing: false});
@@ -53,12 +53,12 @@ export default class Homepage extends Component<Props>{
 
    async getCommits(){
      try {
-       commits = await request('GET /repos/lodash/lodash/commits')
+       commits = await request('GET /repos/inzaw/GMAsses/commits')
        transformedCommits = CommitsTransformer.backward(commits.data)
-       this.setState({loading:false,commits:transformedCommits})
+       this.setState({loading: false, commits: transformedCommits})
      }
      catch(error){
-       this.setState({pageState:PageStates.ERROR,loading:false})
+       this.setState({pageState: PageStates.ERROR, loading: false})
      }
    }
 
@@ -66,14 +66,14 @@ export default class Homepage extends Component<Props>{
     return(<>
       <PageHeader/>
       <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center', paddingHorizontal: 30}}>
-            <View style={{flex:1, width: "100%", height: "100%"}}>
+          <View style={{flex:1, width: "100%", height: "100%"}}>
+            {
               {
-                {
-                  [PageStates.ERROR]: this.renderErrorPage,
-                  [PageStates.AVAILABLE]: this.renderPage,
-                }[this.state.pageState]()
-              }
-            </View>
+                [PageStates.ERROR]: this.renderErrorPage,
+                [PageStates.AVAILABLE]: this.renderPage,
+              }[this.state.pageState]()
+            }
+          </View>
       </SafeAreaView>
         </>);
   }
